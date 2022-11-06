5460ddff591c2a4728477924a2830b528e24f37d
Rawnak Chourasia <rnkchr0@gmail.com>
2022-11-06 19:41:32 +0530

    successfully created a chat socket - basic

--- frontend/chatty/src/App.js
+++ frontend/chatty/src/App.js
@@ -5,6 +5,7 @@ import { nanoid } from "nanoid";
 
 // no dotenv
 const socket = io.connect("http://localhost:5000");
+const userName = nanoid(4);
 
 function App() {
   const [message, setMessage] = useState("");
@@ -12,14 +13,27 @@ function App() {
 
   const sendchat = (e) => {
     e.preventDefault();
-    socket.emit("chat", { message });
+    socket.emit("chat", { message, userName });
     setMessage("");
   };
+
+  useEffect(() => {
+    socket.on("chat", (payload) => {
+      setChat([...chat, payload]);
+    });
+  });
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Chatty App</h1>
+        {chat.map((payload, index) => {
+          return (
+            <p key={index}>
+              {payload.message} <span>id: {payload.userName}</span>
+            </p>
+          );
+        })}
         <form onSubmit={sendchat}>
           <input
             type="text"
